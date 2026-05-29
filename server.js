39f285cc823fa633142ea5555f1a3c389be15d14
dhavarsanam <dhavarsanam11@gmail.com>
2026-05-29 14:44:43 +0530

    update

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.use('/api/menu', require('./routes/menu'));
 app.use('/api/bills', require('./routes/bills'));
 app.use('/api/auth', require('./routes/auth'));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 10000, () => {
+  console.log(`Server running on port ${process.env.PORT || 10000}`);
 });
